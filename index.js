8b1297125577dde2da088091c65e978b2bfdec5f
ivonvk <ivon19950614@gmail.com>
2022-05-22 13:36:58 +0800

    websocket starting point

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const WebSocket = require("ws");
+/*const WebSocket = require("ws");
 
 
 
 
 const wss = new WebSocket.Server({ port: 8000 }, () => {
   console.log("server started");
-});
+});*/
+
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const express = require("express");
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+  const { Server } = require('ws');
+
+  const wss = new Server({ server });
+
+
+
+
+
+
+
+
+
+
 
 
 
