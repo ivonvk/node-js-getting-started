8a3d05f5ea2f6de7ed8da9d4db151ba231ff0606
ivonvk <ivon19950614@gmail.com>
2022-05-22 13:30:52 +0800

    websocket starting point

--- index.js
+++ index.js
@@ -1,12 +1,150 @@
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+const WebSocket = require("ws");
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+
+const wss = new WebSocket.Server({ port: 8000 }, () => {
+  console.log("server started");
+});
+
+
+
+const auth = require("./auth");
+
+let users = [];
+let userlist_ver = 0;
+
+let prefabs = [];
+let prefab_ver = 0;
+
+let timer = 0.0;
+
+setInterval(updateFunc, 10);
+
+function updateFunc() {
+  timer += 0.01;
+  if (timer >= 0.1) {
+    timer = 0.0;
+  }
+}
+
+function findByKey(key, value) {
+  return (item, i) => item[key] === value;
+}
+
+wss.on("connection", (ws) => {
+  ws.on("message", (data) => {
+    const d = JSON.parse(data);
+
+    if (d.method == "msg_add_prefab") {
+      let findParams = findByKey("id", d.id);
+
+      let index = prefabs.findIndex(findParams);
+      if (d.newobj == "true") {
+        let newMovement = {
+          id: prefabs.length,
+          ownerid: d.ownerid,
+          name: d.name,
+          method: d.method,
+          newobj: "false",
+          active: d.active,
+          x: d.x,
+          y: d.y,
+        };
+        prefabs.push(newMovement);
+        ws.send(JSON.stringify(newMovement));
+      } else if (index != -1) {
+        prefabs[index].newobj = "false";
+        ws.send(JSON.stringify(prefabs[index]));
+      } else {
+        let newMovement = {
+          id: d.id,
+          ownerid: d.ownerid,
+          name: d.name,
+          method: d.method,
+          newobj: "false",
+          active: d.active,
+          x: d.x,
+          y: d.y,
+        };
+        prefabs.push(newMovement);
+        ws.send(JSON.stringify(newMovement));
+      }
+    }
+
+    if (d.method == "msg_init_prefab") {
+      let findParams = findByKey("id", d.id);
+
+      let index = prefabs.findIndex(findParams);
+      if (index != -1) {
+      }
+    }
+
+    if (d.method == "msg_update_prefab") {
+      let findParams = findByKey("id", d.id);
+
+      let index = prefabs.findIndex(findParams);
+      if (index != -1) {
+        prefabs[index] = d;
+      }
+    }
+
+    if (d.method == "msg_get_prefab_move") {
+      let findParams = findByKey("id", d.id);
+
+      let index = prefabs.findIndex(findParams);
+      if (index != -1) {
+        ws.send(JSON.stringify(prefabs[index]));
+      }
+    }
+
+    if (d.method == "msg_get_uTimer") {
+      ws.send(timer);
+      return;
+    }
+
+    if (d.method == "user_join") {
+      let findParams = findByKey("name", d.name);
+      let existId = "9999";
+      let index = users.findIndex(findParams);
+      if (index != -1) {
+        existId = index;
+      }
+      if (d.name == "") {
+        ws.send("9999");
+      } else if (index == -1) {
+        let newUser = {
+          id: users.length,
+          name: d.name,
+          method: d.method,
+          active: d.active,
+        };
+        userlist_ver += 1;
+        users.push(newUser);
+
+        ws.send(newUser.id);
+      } else {
+        ws.send(existId);
+      }
+      console.log(d.name + " has joined the server");
+      return;
+    }
+
+    if (d.method == "msg_get_uList") {
+      ws.send(JSON.stringify(users));
+      return;
+    }
+    if (d.method == "msg_get_uList_ver") {
+      ws.send(userlist_ver);
+      return;
+    }
+  });
+});
+
+wss.on("listening", () => {
+  updateFunc();
+  console.log("server is listening on port 8000");
+  auth.demo().then(
+    function(value) {console.log(value);}
+  );
+});
